afa1f03c621c08d8567afdd39bd9afcae424aa39
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-01-09 01:20:59 +0530

    Add files via upload

---
+++ 30 Days Of Code/Abstract Classes.py
@@ -0,0 +1,24 @@
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
+
+#Write MyBook class
+class MyBook(Book):
+    def __init__(self, title, author,price):
+        self.title = title
+        self.author = author
+        self.price = price
+
+    def display(self):
+        print("Title:", title)
+        print("Author:", author)
+        print("Price:", price)
+
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
