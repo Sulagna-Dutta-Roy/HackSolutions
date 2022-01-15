e9e1101dbad3c6376d1c2977527a28fbd5e76560
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-01-15 23:05:38 +0530

    Add files via upload

---
+++ 30 Days Of Code/BitwiseAND.py
@@ -0,0 +1,45 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bitwiseAnd' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER N
+#  2. INTEGER K
+#
+
+def bitwiseAnd(N, K):
+    # Write your code here
+    max = 0
+    for i in range(1,N+1):
+        for j in range(1, i):
+            h = i & j
+            if max < h < K:
+                max = h
+            if max == K - 1:  
+                return max
+    return max          
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        count = int(first_multiple_input[0])
+
+        lim = int(first_multiple_input[1])
+
+        res = bitwiseAnd(count, lim)
+
+        fptr.write(str(res) + '\n')
+
+    fptr.close()
+
