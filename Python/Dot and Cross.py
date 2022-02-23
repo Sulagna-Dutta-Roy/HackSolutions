63675977de72a994442136bf3e3595678163164f
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-02-24 00:06:38 +0530

    Delete Dot and Cross.py

--- Python/Dot and Cross.py
+++
@@ -1,9 +0,0 @@
-import numpy
-
-N = int(input())
-
-A = numpy.array([input().split() for i in range(N)], int)
-B = numpy.array([input().split() for i in range(N)], int)
-
-m = numpy.dot(A, B)
-print(m)
