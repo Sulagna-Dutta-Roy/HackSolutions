ddf9772abf192ba740ee4f99eb95ea9b5de25eb7
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-03-22 19:39:10 +0530

    Add files via upload

---
+++ Python/Common Triplets.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    # Write your code here
+    res = []
+    x=0
+    y=0
+    for i in range(0,len(a)):
+        if a[i]>b[i]:
+            x = x+1
+        elif b[i]>a[i]:
+            y = y +1
+        else:
+            continue
+    res.append(x)
+    res.append(y)
+    return res
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
