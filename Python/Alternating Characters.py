122e49fc01caddabecfc6720451430900d638190
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-03-25 20:48:52 +0530

    Add files via upload

---
+++ Python/Alternating Characters.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    # Write your code here
+    string = list(s)
+    last = string.pop()
+    res = 0
+    
+    while string:
+        newone = string.pop()
+        if newone == last:
+            res += 1
+        else:
+            last = newone
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
