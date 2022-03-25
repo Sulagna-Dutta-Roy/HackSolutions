8fff24f09b303fcaaa21cb70a98dcc6dc73be772
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-03-25 20:55:36 +0530

    Add files via upload

---
+++ Python/Utopian Tree.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    # Write your code here
+    height = 1
+    for growthCycle in range(1, n+1):
+        if growthCycle %2 != 0:
+            height *= 2
+        else:
+            height += 1
+    return height
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
