d63dd2b4c5d8c792afda07e5ce37c78ea5185ef3
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-03-25 20:54:17 +0530

    Add files via upload

---
+++ Python/Beautiful Days.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulDays' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER i
+#  2. INTEGER j
+#  3. INTEGER k
+#
+
+
+def beautifulDays(i, j, k):
+    # Write your code here
+    beautifulDays = 0
+    for day in range(i, j+1):
+        res = int(str(day)[::-1])
+        if abs(res-day)%k ==0:
+            beautifulDays += 1
+    return beautifulDays
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    i = int(first_multiple_input[0])
+
+    j = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
