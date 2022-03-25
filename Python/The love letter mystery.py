214317e60fbd655aea52833774b2d5f6ae26c7ff
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-03-25 20:56:12 +0530

    Add files via upload

---
+++ Python/The love letter mystery.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'theLoveLetterMystery' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def theLoveLetterMystery(s):
+    # Write your code here
+    total = 0
+    for i,j in enumerate(range(len(s)//2),1):
+        a = s[j]
+        b=s[-i]
+        total += abs(ord(a)-ord(b))
+    return total
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = theLoveLetterMystery(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
