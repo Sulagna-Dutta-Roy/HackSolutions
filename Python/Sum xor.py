a9ed178a30b53e3d9bb744bce2a734a5e9dde95d
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-03-28 19:25:11 +0530

    Add files via upload

---
+++ Python/Sum xor.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sumXor' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def sumXor(n):
+    # Write your code here
+    res = 1
+    n_bin = bin(n).replace('0b', '')
+    if (n==0):
+        return 1
+    for digit in n_bin:
+        if digit == '0':
+            res *= 2
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = sumXor(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
