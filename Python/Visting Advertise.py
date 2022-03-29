73ad834c5316c481160e0253e4ffec2e995fef3f
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-03-29 23:53:48 +0530

    Add files via upload

---
+++ Python/Visting Advertise.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    nums = 5
+    peoples = 0
+    
+    for day in range(n):
+        numofPeople = nums//2
+        peoples += numofPeople
+        nums = numofPeople*3
+    return peoples
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
