8975c9fba2b915cf49ea8d9ae8bad67080394807
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-07-14 01:32:57 +0530

    Create Binary Tree Level Order Traversal.py

---
+++ LeetCode-April22/Binary Tree Level Order Traversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans = []
+        
+        def dfs(node, depth):
+            if not node:
+                return
+            if depth >= len(ans):
+                ans.append([])
+            ans[depth].append(node.val)
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+        
+        dfs(root, 0)
+        return ans
