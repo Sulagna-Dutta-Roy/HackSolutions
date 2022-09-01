93a056304e4209af5db49d898d3afde4fa3b843a
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-09-02 01:07:04 +0530

    Create Good nodes.py

---
+++ LeetCode-April22/Good nodes.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        def dfs(node, curMax):
+            if not node:
+                return
+            if node.val >= curMax:
+                count[0] += 1
+                curMax = node.val
+            dfs(node.left, curMax)
+            dfs(node.right, curMax)
+        
+        count = [0]
+        dfs(root, root.val)
+        
+        return count[0]
