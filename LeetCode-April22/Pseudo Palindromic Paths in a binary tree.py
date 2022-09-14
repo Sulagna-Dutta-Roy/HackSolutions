be18fbe0cc4aaedbf12bebee32f374de61225eae
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-09-15 01:03:20 +0530

    Create Pseudo Palindromic Paths in a binary tree.py

---
+++ LeetCode-April22/Pseudo Palindromic Paths in a binary tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        def traverse(node, pairs):
+            if not node:
+                return 0
+            
+            if node.val in pairs:
+                pairs.remove(node.val)
+            else:
+                pairs.add(node.val)
+            
+            if not node.left and not node.right:
+                return 1 if len(pairs) <= 1 else 0
+            
+            left = traverse(node.left, set(pairs))
+            right = traverse(node.right, set(pairs))
+            
+            return left + right
+        
+        return traverse(root, set())
