989f4a0244aa7a40ea42ef93756ff2fedeb35e69
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-11-14 01:50:52 +0530

    Create Plus One.java

---
+++ Leetcode-November/Plus One.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+    for(int i=n-1; i>=0; i--) {
+        if(digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    int[] num = new int [n+1];
+    num[0] = 1;
+    return num;
+    }
+}
