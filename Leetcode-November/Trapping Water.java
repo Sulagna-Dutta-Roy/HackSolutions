f0ccf2b59271d71613d98dbe826a209c5fcee66c
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-11-24 01:50:24 +0530

    Create Trapping Water.java

---
+++ Leetcode-November/Trapping Water.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] leftMax = new int[n], rightMax = new int[n];
+        for (int i = 1; i < n; ++i)
+            leftMax[i] = Math.max(height[i-1], leftMax[i-1]);
+        for (int i = n-2; i >= 0; --i)
+            rightMax[i] = Math.max(height[i+1], rightMax[i+1]);
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            if (waterLevel >= height[i]) ans += waterLevel - height[i];
+        }
+        return ans;
+    }
+}
