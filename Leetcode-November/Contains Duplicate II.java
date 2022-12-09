0582b136792803c6e3e1a043ba8d05a2c7a24bad
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-12-09 16:48:24 +0530

    Create Contains Duplicate II.java

---
+++ Leetcode-November/Contains Duplicate II.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i< nums.length;i++) {
+            if(map.containsKey(nums[i])){ if (Math.abs(i-map.get(nums[i]))<=k) return true;}
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
