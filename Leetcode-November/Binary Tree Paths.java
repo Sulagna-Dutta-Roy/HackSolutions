c112da9a9cd23cd75da95a97ab7dc5adec78a90d
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2022-12-17 02:24:25 +0530

    Create Binary Tree Paths.java

---
+++ Leetcode-November/Binary Tree Paths.java
@@ -0,0 +1,35 @@
+// Definition for a binary tree node.
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+ }
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new LinkedList<>();
+
+        if(root == null) return paths;
+        
+        if(root.left == null && root.right == null){
+            paths.add(root.val+"");
+            return paths;
+        }
+
+         for (String path : binaryTreePaths(root.left)) {
+             paths.add(root.val + "->" + path);
+         }
+
+         for (String path : binaryTreePaths(root.right)) {
+             paths.add(root.val + "->" + path);
+         }
+
+         return paths;
+    }
+}
