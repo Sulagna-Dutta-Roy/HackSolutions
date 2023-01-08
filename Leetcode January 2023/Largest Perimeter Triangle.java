869af848439929e53fb847426f5eb251135bce98
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-01-08 17:24:35 +0530

    Create Largest Perimeter Triangle.java

---
+++ Leetcode January 2023/Largest Perimeter Triangle.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = n-3; i >= 0; i --){
+            if(nums[i] + nums[i+1] > nums[i+2]){
+                return nums[i] + nums[i+1] + nums[i+2];
+            }   
+        }
+        return 0;
+    }
+}
