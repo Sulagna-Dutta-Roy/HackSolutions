a16f90ab23abe14428cc30e17f2c199112b2e35e
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-03-09 02:08:17 +0530

    Create KOKO eating bananas.java

---
+++ Leetcode March 2023/KOKO eating bananas.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low=1;
+        int high=Integer.MIN_VALUE;
+        for(int i=0;i<piles.length;i++){
+            high=Math.max(high,piles[i]);
+        }
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(blackbox(mid,piles,h)){
+                high=mid;
+            }
+            else
+                low=mid+1;
+        }
+        return low;
+    }
+    public boolean box(int maxpiles,int[] piles,int h){
+        int hours=0;
+        for(int i:piles){
+            int time=i/maxpiles;
+            hours+=time;
+            if(i%maxpiles!=0) hours++;
+        }
+        if(hours<=h)
+            return true;
+        return false;
+    }
+}
