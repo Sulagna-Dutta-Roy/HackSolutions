1f4a3bfe768f0f2dfa6de93865190fe07d6c2b24
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-04-29 02:18:51 +0530

    Create Similar String Groups.java

---
+++ Leetcode April 2023/Similar String Groups.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (areSimilar(strs[i], strs[j])) {
+                    int parentI = find(parent, i);
+                    int parentJ = find(parent, j);
+                    if (parentI != parentJ) {
+                        parent[parentJ] = parentI;
+                    }
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    private boolean areSimilar(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+                if (diff > 2) {
+                    return false;
+                }
+            }
+        }
+        return diff == 2 || diff == 0;
+    }
+    
+    private int find(int[] parent, int i) {
+        if (parent[i] != i) {
+            parent[i] = find(parent, parent[i]);
+        }
+        return parent[i];
+    }
+}
