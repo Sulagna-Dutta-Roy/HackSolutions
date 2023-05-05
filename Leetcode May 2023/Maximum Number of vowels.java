1ff47b2cebcfeda6fa76c1288f619222c4d27b3a
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-05-06 01:42:34 +0530

    Create Maximum Number of vowels.java

---
+++ Leetcode May 2023/Maximum Number of vowels.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxVowels(String s, int k) {
+       int n = s.length();
+        int maxVowels = 0;
+        int count = 0;
+
+        int[] vowels = new int[128];
+        vowels['a'] = 1;
+        vowels['e'] = 1;
+        vowels['i'] = 1;
+        vowels['o'] = 1;
+        vowels['u'] = 1;
+
+        for (int i = 0; i < k; i++) {
+            count += vowels[s.charAt(i)];
+        }
+
+        maxVowels = count;
+        for (int i = k; i < n; i++) {
+            count += vowels[s.charAt(i)] - vowels[s.charAt(i - k)];
+            maxVowels = Math.max(maxVowels, count);
+            //System.out.println(Arrays.toString(vowels));
+            if (maxVowels == k) {
+                return maxVowels; 
+            }
+        }
+        return maxVowels; 
+    }
+}
