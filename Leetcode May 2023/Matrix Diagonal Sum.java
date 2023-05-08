939b2b895e87814c72e70c6ad906d6d50f8d2ab5
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-05-08 22:09:20 +0530

    Create Matrix Diagonal Sum.java

---
+++ Leetcode May 2023/Matrix Diagonal Sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result += mat[i][i] + mat[i][n - i - 1];
+        }
+        if (n % 2 == 1) {
+            result -= mat[n / 2][n / 2];
+        }
+        return result;  
+    }
+}
