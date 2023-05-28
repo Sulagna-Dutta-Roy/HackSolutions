8f879d2de73761dc1177040cbe4525c537b8ba5c
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-05-29 02:02:37 +0530

    Create Minimum cost to cut a stick.java

---
+++ Leetcode May 2023/Minimum cost to cut a stick.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minCost(int n, int[] cuts) {
+       List<Integer> A = new ArrayList<>();
+        for (int a : cuts) {
+            A.add(a);
+        }
+        A.add(0);
+        A.add(n);
+        Collections.sort(A);
+        int k = A.size();
+        int[][] dp = new int[k][k];
+        for (int d = 2; d < k; ++d) {
+            for (int i = 0; i < k - d; ++i) {
+                dp[i][i + d] = 1000000000;
+                for (int m = i + 1; m < i + d; ++m) {
+                    dp[i][i + d] = Math.min(dp[i][i + d], dp[i][m] + dp[m][i + d] + A.get(i + d) - A.get(i));
+                }
+            }
+        }
+        return dp[0][k - 1];  
+    }
+}
