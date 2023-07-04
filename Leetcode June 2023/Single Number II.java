f6b131d6a7487a18627efb67a9ef5174cf68eb2d
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-07-04 23:23:26 +0530

    Create Single Number II.java

---
+++ Leetcode June 2023/Single Number II.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+       int res = 0;
+       for(int i=0;i<32;i++) {
+           int sum = 0;
+           for(final int num:nums) 
+              sum += num >> i & 1;
+            sum %= 3;
+            res |= sum << i;
+       }
+    return res;
+    }
+}
