ac5ec5c0d542f137f556f9dcdfa947b281bd1f94
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-07-15 21:03:00 +0530

    Create Missing number in Array.java

---
+++ GFG POTD 2023/Missing number in Array.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] array = new int[n - 1];
+            for (int i = 0; i < n - 1; i++) {
+                array[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            System.out.println(sln.missingNumber(array, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int missingNumber(int array[], int n) {
+       Arrays.sort(array);
+       for(int i=0;i<array.length;i++) {
+           if(i+1 != array[i]) {
+               return i+1;
+           }
+       }
+     return array[n-2]+1;
+    }
+}
