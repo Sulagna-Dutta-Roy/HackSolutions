7cdf8d63c4bb7cc5b9f772a6951b513d8c23a0d0
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-07-21 02:13:14 +0530

    Create Reverse a linked list.java

---
+++ GFG POTD 2023/Reverse a linked list.java
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize
+{
+    static Node head;
+    
+    public static void main (String[] args)throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
+            int a1 = Integer.parseInt(s[0]);
+            Node head = new Node(a1);
+            Node tail = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = Integer.parseInt(s[i]);
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail =tail.next;
+            }
+            
+            int k = Integer.parseInt(in.readLine().trim());
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res,out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node node,PrintWriter out)
+    {
+        while(node != null)
+        {
+            out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+}
+
+
+// } Driver Code Ends
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution
+{
+    public static Node reverse(Node node, int k)
+    {
+        Node curr = node;
+        Node prev = null;
+        Node next = null;
+        int count=0;
+        while(curr != null && count<k) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+    if(next != null) {
+        node.next = reverse(next,k);
+     }
+    return prev;
+    }
+}
+
