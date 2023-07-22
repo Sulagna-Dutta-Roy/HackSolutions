399827843fb43045c3f389432c360898bb11a4da
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-07-22 22:13:14 +0530

    Create Badversion.java

---
+++ Leetcode June 2023/Badversion.java
@@ -0,0 +1,19 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+       int mid =0;
+       int high = n;
+       int low =0;
+       while(low<=high) {
+        mid = low + (high - low)/2;
+        if(isBadVersion(mid)) {
+            high = mid-1;
+        } else {
+          low = mid+1;
+       } 
+    }
+    return low;
+  }
+}
