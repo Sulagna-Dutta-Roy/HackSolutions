7bfe998391622c1dd246d3e52062415e7cb4c7dc
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-08-11 23:20:21 +0530

    Create Coin change.java

---
+++ GFG POTD 2023/Coin change.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int sum = sc.nextInt();
+            int N = sc.nextInt();
+            int coins[] = new int[N];
+            for (int i = 0; i < N; i++) coins[i] = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.println(ob.count(coins, N, sum));
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public long count(int coins[], int N, int sum) {
+        long[] ab = new long[sum+1];
+        ab[0] = 1;
+        
+        for(int coin:coins) {
+            for(int i=coin;i<=sum;i++) {
+                ab[i] += ab[i-coin];
+            }
+        }
+    return ab[sum];
+    }
+}
