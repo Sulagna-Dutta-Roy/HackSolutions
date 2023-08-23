51f7a16fa0e891592472b6df85e76a77ae3f5eab
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-08-24 01:30:11 +0530

    Create Multiply two strings.java

---
+++ GFG POTD 2023/Multiply two strings.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.math.*;
+
+class Multiply{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            String a=sc.next();
+            String b=sc.next();
+            Solution g=new Solution();
+            System.out.println(g.multiplyStrings(a,b));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String multiplyStrings(String s1,String s2)
+    {
+        BigInteger a = new BigInteger(s1);
+        BigInteger b = new BigInteger(s2);
+        BigInteger multiply = a.multiply(b);
+        return multiply.toString();
+    }
+}
