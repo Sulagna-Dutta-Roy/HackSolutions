8e5a8702213946cc0ece795993d7f4ae62de4679
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-10-20 21:33:58 +0530

    Create Array 3 digits.java

---
+++ GFG POTD 2023/Array 3 digits.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(in.readLine());
+            String a[] = in.readLine().trim().split("\\s+");
+            int arr[] = new int[N];
+            for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(a[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.isPossible(N, arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int isPossible(int N, int arr[]) {
+        int rem = 0;
+        for(int i : arr){
+            rem = (rem+i)%3;
+        }
+        return rem==0?1:0;
+    }
+}
