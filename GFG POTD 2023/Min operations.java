5ffe718c043653c603b03ce0924814ab9c21e2d4
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-10-26 23:36:25 +0530

    Create Min operations.java

---
+++ GFG POTD 2023/Min operations.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minOperation(n));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    public int minOperation(int n)
+    {
+        if(n == 1){
+             return 1;
+         }
+         
+         int count = 0;
+         while(n > 0){
+             if(n % 2 == 0){
+                n = n >> 1;
+             }else{
+                n -= 1; 
+             }
+             
+             count++;
+         }
+         
+         return count;
+    }
+}
