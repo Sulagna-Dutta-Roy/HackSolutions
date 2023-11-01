0704a53184b030ada5ec4885be072bed4d79da5e
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-11-01 22:24:20 +0530

    Create Frequency count.java

---
+++ GFG POTD 2023/Frequency count.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+        int t = Integer.parseInt(br.readLine().trim()); 
+        while(t-->0){
+            
+            //size of array
+            int N = Integer.parseInt(br.readLine().trim()); 
+            int arr[] = new int[N];
+            String inputLine[] = br.readLine().trim().split(" ");
+
+            //adding elements to the array
+            for(int i = 0 ; i < N; i++){
+                arr[i]=Integer.parseInt(inputLine[i]); 
+            }
+            int P= Integer.parseInt(br.readLine().trim());
+            //calling frequncycount() function
+            Solution ob = new Solution();
+            ob.frequencyCount(arr, N, P); 
+            
+            //printing array elements
+            for(int i = 0; i < N ; i++)
+                System.out.print(arr[i] + " " );
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to count the frequency of all elements from 1 to N in the array.
+    public static void frequencyCount(int arr[], int N, int P)
+    {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<N;i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        }
+        for(int i=0;i<N;i++){
+            if(map.get(i+1) != null)
+                arr[i] = map.get(i+1);
+            else
+                arr[i] = 0;
+        }
+    }
+}
