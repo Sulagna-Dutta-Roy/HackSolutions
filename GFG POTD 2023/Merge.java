98bb4272985cdb3c1fe0b517f0be55601a30f8c6
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-11-19 23:12:59 +0530

    Create Merge.java

---
+++ GFG POTD 2023/Merge.java
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node 
+{
+    int data;
+    Node next;
+    
+    Node(int a)
+        {
+            data = a;
+            next = null;
+        }
+}
+class GfG
+{
+    static Scanner sc = new Scanner(System.in);
+    public static Node inputList(int size)
+    {
+        Node head, tail;
+        int val;
+        
+        val = sc.nextInt();
+        
+        head = tail = new Node(val);
+        
+        size--;
+        
+        while(size-->0)
+        {
+            val = sc.nextInt();
+            tail.next = new Node(val);
+            tail = tail.next;
+        }
+        
+        return head;
+    }
+    
+    public static void printList(Node n)
+    {
+        while(n!=null)
+        {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+    
+    public static void main(String args[])
+        {
+            
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n , m;
+                    
+                    n = sc.nextInt();
+                    m = sc.nextInt();
+                    
+                    Node head1 = inputList(n);
+                    Node head2 = inputList(m);
+                    
+                    Solution obj = new Solution();
+                    
+                    Node result = obj.findIntersection(head1, head2);
+	    
+	                printList(result);
+	                System.out.println();
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution
+{
+    public static void fun(Node res, Node head1, Node head2) {
+        if(head1==null || head2==null) return;
+        if(head1.data == head2.data) {
+            res.next = new Node(head1.data);
+            fun(res.next, head1.next, head2.next);
+        } else if(head1.data < head2.data) {
+            fun(res, head1.next, head2);
+        } else {
+            fun(res, head1, head2.next);
+        }
+    }
+   public static Node findIntersection(Node head1, Node head2)
+    {
+        // code here.
+        Node res = new Node(-1);
+        fun(res, head1, head2);
+        return res.next;
+    }
+}
