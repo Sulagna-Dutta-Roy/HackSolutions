d2447bb579f753aee63cf7d2d6b8713185a5b437
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-12-23 16:36:36 +0530

    Create Count occurrence.java

---
+++ GFG POTD 2023/Count occurrence.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class Driverclass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int k = sc.nextInt();
+
+            System.out.println(new Solution().countOccurence(arr, n, k));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution 
+{
+    //Function to find all elements in array that appear more than n/k times.
+    public int countOccurence(int[] arr, int n, int k) 
+    {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++) map.put(arr[i],map.getOrDefault(arr[i],0) + 1);
+        return (int) map.entrySet().stream().filter(num -> num.getValue() > n/k).count();
+    }
+}
