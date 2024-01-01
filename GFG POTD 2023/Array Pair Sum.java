9b3172f7ec19f62f43b23eacc22674a2b117fff8
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2024-01-01 22:15:28 +0530

    Create Array Pair Sum.java

---
+++ GFG POTD 2023/Array Pair Sum.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s1 = br.readLine().trim();
+            String[] S1 = s1.split(" ");
+            int n = Integer.parseInt(S1[0]);
+            int k = Integer.parseInt(S1[1]);
+            String s2 = br.readLine().trim();
+            String[] S2 = s2.split(" ");
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) {
+                nums[i] = Integer.parseInt(S2[i]);
+            }
+            Solution ob = new Solution();
+            boolean ans = ob.canPair(nums, k);
+            if (ans)
+                System.out.println("True");
+            else
+                System.out.println("False");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public boolean canPair(int[] nums, int k) {
+        if(nums.length%2==1) return false;
+        Set<Integer> set = new HashSet<>();
+        for(int i: nums){
+            int mod = i%k;
+            if(mod==0){if(!set.add(0)) set.remove(0);}
+            else if(set.contains(k-mod)) set.remove(k-mod);
+            else set.add(mod);
+        }
+        return set.isEmpty();
+    }
+}
