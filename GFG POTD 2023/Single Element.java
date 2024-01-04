4aab8a72d365a0f8395eb3ffdbef8babc7f11efe
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2024-01-04 23:37:06 +0530

    Create Single Element.java

---
+++ GFG POTD 2023/Single Element.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for JAVA
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            
+            int[] arr = new int[N];
+            
+            for(int i=0 ; i<N ; i++)
+                arr[i] = Integer.parseInt(S[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.singleElement(arr,N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int singleElement(int[] arr , int N) {
+        Map <Integer, Integer> m = new HashMap<>();
+        
+       for(int i:arr){
+           m.put(i, m.getOrDefault(i,0)+1);
+       }
+       
+       
+       
+       for(int i:arr){
+           if(m.get(i) == 1){
+               return i;
+           }
+       }
+       
+       return -1;
+    }
+}
