4c279dff862fb3a3477eb6a4eb292e74b92be221
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2024-01-08 23:01:22 +0530

    Create Merge 2.java

---
+++ GFG POTD 2023/Merge 2.java
@@ -0,0 +1,120 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t=scanner.nextInt();
+        while(t-->0)
+        {
+            int N = scanner.nextInt();
+            int M = scanner.nextInt();
+
+            Node node1 = null;
+            Node temp1 = null;
+            for (int i = 0; i < N; i++) {
+                int value = scanner.nextInt();
+                Node newNode = new Node(value);
+                if (node1 == null) {
+                    node1 = newNode;
+                    temp1 = node1;
+                } else {
+                    temp1.next = newNode;
+                    temp1 = temp1.next;
+                }
+            }
+    
+            Node node2 = null;
+            Node temp2 = null;
+            for (int i = 0; i < M; i++) {
+                int value = scanner.nextInt();
+                Node newNode = new Node(value);
+                if (node2 == null) {
+                    node2 = newNode;
+                    temp2 = node2;
+                } else {
+                    temp2.next = newNode;
+                    temp2 = temp2.next;
+                }
+            }
+    
+            GfG gfg = new GfG();
+            Node result = gfg.mergeResult(node1, node2);
+    
+            printList(result);
+        }
+    }
+
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/* Structure of the node*/
+/* class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+} */
+
+class GfG
+{
+    Node mergeResult(Node node1, Node node2)
+    {
+	   Node prev = null;
+	    Node temp = null;
+	    while(node1!=null &&node2!=null){
+	        if(node1.data<=node2.data){
+	            temp = node1;
+	            node1 = node1.next;
+	        }else{
+	            temp = node2;
+	            node2 = node2.next;
+	        }
+	        temp.next = prev;
+	        prev = temp;
+	    }
+	    while(node1!=null){
+	        temp = node1;
+	        node1 = node1.next;
+	        temp.next = prev;
+	        prev = temp;
+	    }
+	    
+	    while(node2!=null){
+	        temp = node2;
+	        node2 = node2.next;
+	        temp.next = prev;
+	        prev = temp;
+	    }
+	    
+	    return prev;
+    }
+}
