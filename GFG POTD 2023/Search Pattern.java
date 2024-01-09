1e40a8ec8cccf0add4e915e5ddf719bd594ad934
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2024-01-09 23:34:53 +0530

    Create Search Pattern.java

---
+++ GFG POTD 2023/Search Pattern.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            String s, patt;
+            s = sc.next();
+            patt = sc.next();
+            
+            Solution ob = new Solution();
+            
+            ArrayList<Integer> res = ob.search(patt, s);
+            if(res.size()==0)
+                System.out.print(-1);
+            else {
+                for(int i = 0;i<res.size();i++)
+                    System.out.print(res.get(i) + " ");
+            }
+            System.out.println();    
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    
+    ArrayList<Integer> search(String pat, String txt)
+    {
+        ArrayList<Integer>A=new ArrayList<>();
+        int index=txt.indexOf(pat);
+        while(index!=-1){
+            A.add(index+1);
+            index=txt.indexOf(pat,index+1);
+        }
+        return A;
+    }
+}
