ba5754ad76c31a579fada54a6c99a7c76e571c1a
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2024-01-14 22:32:02 +0530

    Create Repeated rows.java

---
+++ GFG POTD 2023/Repeated rows.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int m = Integer.parseInt(inputLine[1]);
+            int[][] arr = new int[n][m];
+            
+            for(int i = 0; i < n; i++){
+                String[] s = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++){
+                    arr[i][j] = Integer.parseInt(s[j]);
+                }
+            }
+            ArrayList<Integer> ans = new Solution().repeatedRows(arr, n, m);
+            for(int i=0;i<ans.size();i++)
+                System.out.print(ans.get(i)+" ");
+                
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static ArrayList<Integer> repeatedRows(int matrix[][], int m, int n)
+    {
+        HashSet<String> map=new HashSet<>();
+        ArrayList<Integer> res=new ArrayList<>();
+        for(int i=0;i<m;i++){
+            StringBuilder row=new StringBuilder();
+            for(int j=0;j<n;j++){
+                row.append(matrix[i][j]);
+            }
+            if(map.contains(row.toString())) res.add(i);
+            else map.add(row.toString());
+        }
+        return res;
+    }
+}
