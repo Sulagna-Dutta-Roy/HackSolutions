ad86aaf08f6e5c9c7d7fbb9fe1253249c2d521f0
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2024-01-16 23:04:11 +0530

    Create Sequence of sequence.java

---
+++ GFG POTD 2023/Sequence of sequence.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int m = Integer.parseInt(input_line[0]);
+            int n = Integer.parseInt(input_line[1]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.numberSequence(m, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int numberSequence(int m, int n)
+    {
+if(m<n) return 0;
+        
+        if(n==0) return 1;
+        
+        return numberSequence(m-1,n)+numberSequence(m/2,n-1);
+    }
+}
