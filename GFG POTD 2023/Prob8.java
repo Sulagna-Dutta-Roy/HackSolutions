ab301fa0e65109571c553aea01dbefe295f58b51
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2024-02-08 23:37:11 +0530

    Create Prob8.java

---
+++ GFG POTD 2023/Prob8.java
@@ -0,0 +1,143 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+// INITIAL CODE
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// A Binary Tree node
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class Is_Leaves_At_Same_Level
+{
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+
+    // driver function to test the above functions
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t=Integer.parseInt(br.readLine());
+        
+        while(t > 0)
+        {
+            String s = br.readLine();
+            Node root = buildTree(s);
+        
+            Solution g = new Solution();
+	    	boolean b = g.check(root);
+		   	if(b == true)
+		    	System.out.println(1);
+	    	else
+		   	    System.out.println(0);
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+class Solution
+{
+    boolean check(Node root)
+    {
+       return helper(root,1);
+    }
+    int h = 0;
+    boolean first = true;
+    private boolean helper(Node root, int height){
+        if(root == null) return true;
+        if(root.left == root.right){
+            if(first){
+                h=height;    
+                first = false;
+            }
+            else if(height != h) return false;
+            return true;
+        }
+        
+        return helper(root.left, height+1) && helper(root.right,height+1); 
+    }
+}
